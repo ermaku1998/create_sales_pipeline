a264047d9ceb1a1863ed3966cd3535382000c55c
ermaku1998 <102954780+ermaku1998@users.noreply.github.com>
2022-08-26 23:56:31 +0300

    Update uploading_sales.py

--- dags/utils/uploading_sales.py
+++ dags/utils/uploading_sales.py
@@ -24,15 +24,52 @@ def drop_pmix_sales():
         rs = con.execute('''
                             DROP TABLE pmix_sales;
                          ''')
+# Дропаем старый dates_last40
+def drop_dates_last40():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE dates_last40;
+                         ''')
+# Делаем drop assort_matrix
+def drop_assort_matrix():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE assort_matrix_history;
+                         ''')
+# Дропаем прайсовую матрицу
+def drop_price_hist():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE price_history;
+                         ''')
+# Дропаем skelet_assort_last40
+def drop_assort_last40():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE skelet_assort_last40;
+                         ''')  
+# Дропаем skelet_prhist_last40
+def drop_prhist_last40():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE skelet_prhist_last40;
+                         ''')
+# Дропаем skelet_assort_prhist_last40
+def drop_assort_prhist_last40():
+    with postgre_engine.connect() as con:
+        rs = con.execute('''
+                            DROP TABLE skelet_assort_prhist_last40;
+                         ''')
 
-
-# Создаём генератор pmix_sales и выгружаем
-def upload_pmix_sales():
+        
+# Выгружаем таблицы из оракла
+def from_oracle_to_postgre():
+    # Импортируем pmix_sales
     qry = f'''
               SELECT product_id, pbo_location_id, sales_dt, gross_sales_amt, 
                      gross_sales_amt_discount, sales_qty, sales_qty_discount
               FROM SAS_INTERF.IA_PMIX_SALES
-              WHERE CHANNEL_CD = 'ALL' and sales_dt <> current_date
+              WHERE CHANNEL_CD = 'ALL' and sales_dt <> current_date;
            '''
     table_generator = pd.read_sql_query(qry, oracle_engine, chunksize = 1500000)
  
@@ -42,54 +79,8 @@ def upload_pmix_sales():
         srtd_table['sales_dt'] = pd.to_datetime(srtd_table['sales_dt'])
         srtd_table.to_sql('pmix_sales', postgre_engine, if_exists='append', index=False)
 
-
-# Дропаем старый dates_last40
-def drop_dates_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE dates_last40;
-                         ''')
-
-
-# Создаём столбец с датами за последние 40 дней
-def create_dates_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            CREATE TABLE dates_last40 as
-                            SELECT distinct sales_dt as dt
-                            FROM pmix_sales
-                         ''')
-
-
-# Удаляем строки из pmix_sales_history за последние 40 дней
-def delete_from_pmix_sales_hist():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DELETE FROM pmix_sales_history
-                            WHERE sales_dt >= (select min(dt) from dates_last40)
-                         ''')
-
-
-# Добавляем эти строки в pmix_sales_history
-def add_to_pmix_sales_hist():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            INSERT INTO pmix_sales_history
-                            SELECT *
-                            FROM pmix_sales
-                         ''')
-
-
-# Делаем drop assort_matrix
-def drop_assort_matrix():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE assort_matrix_history;
-                         ''')
-
-
-# Импортируем ассортиментную таблицу
-def upload_assort_matrix():
+        
+    # Импортируем ассортиментную таблицу
     qry = f'''
     SELECT *
     FROM SAS_INTERF.IA_ASSORT_MATRIX_HISTORY
@@ -98,18 +89,9 @@ def upload_assort_matrix():
 
     for table in tqdm(table_generator):
         table.to_sql('assort_matrix_history', postgre_engine, if_exists='append', index=False)
-
-
-# Дропаем прайсовую матрицу
-def drop_price_hist():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE price_history;
-                         ''')
         
         
-# Импортируем прайсовую таблицу
-def upload_price_hist():
+    # Импортируем прайсовую таблицу
     def lower_clmns_names(clmns: list) -> dict:
         rnm_clmns = [clmn.lower() for clmn in clmns]
         rnm_clmns_d = dict(zip(clmns, rnm_clmns))
@@ -128,77 +110,53 @@ def upload_price_hist():
         table['end_dt'] = pd.to_datetime(table['end_dt'])
 
         table.to_sql('price_history', postgre_engine, if_exists='append', index=False)
-
-
-# Дропаем skelet_assort_last40
-def drop_assort_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE skelet_assort_last40
-                         ''') 
-
-
+        
+        
+        
+# Проводим работу с таблицами в postgre:
+# Создаём столбец с датами за последние 40 дней
+# Удаляем строки из pmix_sales_history за последние 40 дней
+# Добавляем эти строки в pmix_sales_history
 # Создаем skelet_assort_last40 как ассортиментная матрица за последние 40 дней
-def create_assort_last40():
+# Создаем skelet_prhist_last40
+# Создаем таблицу из ассортиментной и прайсовой с уникальными продукт-пбо-дата за последние 40 дней
+# Удаляем строки из sales за последние 40 дней
+def creating():
     with postgre_engine.connect() as con:
         rs = con.execute('''
+                            CREATE TABLE dates_last40 as
+                            SELECT distinct sales_dt as dt
+                            FROM pmix_sales;
+                            
+                            DELETE FROM pmix_sales_history
+                            WHERE sales_dt >= (select min(dt) from dates_last40);
+                            
+                            INSERT INTO pmix_sales_history
+                            SELECT *
+                            FROM pmix_sales;
+                            
                             CREATE TABLE skelet_assort_last40 as
                             SELECT product_id, pbo_location_id, dt
                             FROM assort_matrix_history INNER JOIN dates_last40
                                  ON dt BETWEEN start_dt and end_dt
                             ORDER BY dt;
-                         ''')
-
-        
-# Дропаем skelet_prhist_last40
-def drop_prhist_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE skelet_prhist_last40
-                         ''')
-
-
-# Создаем skelet_prhist_last40
-def create_prhist_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
+                            
                             CREATE TABLE skelet_prhist_last40 as
                             SELECT product_id, pbo_location_id, dt, gross_price_amt
                             FROM  price_history INNER JOIN dates_last40
                                  ON dt BETWEEN start_dt and end_dt
                             ORDER BY dt;
-                         ''')
-
-
-# Дропаем skelet_assort_prhist_last40
-def drop_assort_prhist_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
-                            DROP TABLE skelet_assort_prhist_last40
-                         ''')
-
-
-# Создаем таблицу из ассортиментной и прайсовой с уникальными продукт-пбо-дата за последние 40 дней
-def create_assort_prhist_last40():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
+                            
                             CREATE TABLE skelet_assort_prhist_last40 as
                             SELECT a.product_id, a.pbo_location_id, a.dt, gross_price_amt
                             FROM  skelet_assort_last40 a LEFT JOIN skelet_prhist_last40 p
                                  ON a.product_id = p.product_id and
                                     a.pbo_location_id = p.pbo_location_id and
                                     a.dt = p.dt;
-                         ''')
-
-
-# Удаляем строки из sales за последние 40 дней
-def delete_from_sales():
-    with postgre_engine.connect() as con:
-        rs = con.execute('''
+                                    
                             DELETE FROM sales
-                            WHERE dt >= (select min(dt) from dates_last40)
+                            WHERE dt >= (select min(dt) from dates_last40);
                          ''')
-
 
 # Создаем итоговую таблицу из ассортиментной, прайсовой и pmix с уникальными продукт-пбо-дата
 def add_to_sales():
